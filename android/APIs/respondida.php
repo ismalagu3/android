1f96485f7e5be03d927e820505ed88b4107ca45a
ismalagu3 <91255139+ismalagu3@users.noreply.github.com>
2022-05-31 22:12:22 +0200

    Delete android directory

--- android/APIs/respondida.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET["id"];
-$mysqli =new mysqli("localhost", "root", "", "android");
-$consulta = "UPDATE preguntas set respondido='si' where id like $id";
-$res=$mysqli->query($consulta);
-
-header('Location: ../subpaginas/web.php');
-
-?>
--- android/js/timer.js
+++
@@ -1,5 +0,0 @@
-$(function (e){
-    setInterval(function () {
-        $(location).attr('href',"web.php")
-    }, 10000);
-})
